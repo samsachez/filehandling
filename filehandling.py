13d93d6bdb6209e7b364adc9cde6d2c7943ce026
samsachez <samuelmusyoka003@gmail.com>
2024-04-08 14:35:06 +0300

    first commit

---
+++ filehandling.py
@@ -0,0 +1,17 @@
+#creating the file
+#f1 = open('flash.txt','w')
+#adding info to the file
+#f1.write('samuel\n')
+#f1.write('200 is a big number\n')
+#f1.write('sachez\n')
+#printing flash.txt file.
+#file = open('flash.txt','r')
+#content = file.read()
+#print(content)
+#file.close()
+file = open('flash.txt', 'a')
+file.write('wisely alone\n')
+file.write('wisely together\n')
+file.write('wisely forever\n')
+file.close()
+
